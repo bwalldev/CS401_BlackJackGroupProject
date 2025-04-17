258eb7560f704a8c1f9fa88200a9225323275159
Brandon Wall <brandon.wall.dev@gmail.com>
2025-04-16 21:42:42 -0700

    Started coding the Message class
    Needs work, this is just the initial version.

--- BlackjackGame/src/Message.java
+++ BlackjackGame/src/Message.java
@@ -1,4 +1,33 @@
+import java.io.Serializable;
+import java.util.Date;
 
-public class Message {
-
+public class Message implements Serializable {
+	private static int idCount = 0;
+	private final int id;
+	private final String type;
+	private final String text;
+	private final Date date;
+	
+	public Message(String text, String type) {
+		this.id = ++this.idCount;
+		this.type = type;
+		this.text = text;
+		this.date = new Date();
+	}
+	
+	public int getID() {
+		return this.id;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+	
+	public String getText() {
+		return this.text;
+	}
+	
+	public Date getDate() {
+		return this.date;
+	}
 }
