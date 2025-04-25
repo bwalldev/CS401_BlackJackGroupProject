db6f4166fc6d3967315c84e69ce8aa7dcfdf5174
Mahlet2425 <Mahletsolomon138@gmail.com>
2025-04-24 20:52:00 -0700

    updated client and dealer class

--- BlackjackGame/src/Client.java
+++ BlackjackGame/src/Client.java
@@ -1,4 +1,36 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Client {
+    private Socket socket;                  
+    private BufferedReader input;          
+    private PrintWriter output;            
+    private Player player;                 
+    private Game game;                     
 
+    //connect the client to the server
+    public void connectToServer(String serverAddress, int port) {
+       
+    }
+
+  
+    public void send(String message) {
+       
+    }
+
+    public String receive(){
+    	
+		return null;
+        
+    }
+
+    //sends players action ( hit, stand)
+    public void sendAction(String action) {
+       
+    }
+
+    public void disconnect() {
+       
+    }
 }
--- BlackjackGame/src/Dealer.java
+++ BlackjackGame/src/Dealer.java
@@ -1,4 +1,54 @@
-
 public class Dealer {
-
+	//Attributes 
+   private int id;
+   private String username;
+   private String Password;
+   private Hand hand;
+ 
+   
+   //constructor
+   public Dealer(int id, String username, String password) {
+	   this.id = 0;
+	   this.username = "Dealer";
+	   this.Password = "password";
+	   this.hand = new Hand();
+	  }
+   
+   //getter methods
+   public int getID() {
+	   return id;
+   }
+   
+   public String getUsername() {
+	   return username;
+   }
+	  
+   public String getPassword() {
+	   return Password;
+   }
+   
+   public Hand getHand() {
+	   return hand;
+   }
+   
+   public int getHandvalue() {
+	   return hand.getHandTotal();
+   }
+   
+   public void addCardToHand(Card newcard) {
+	   hand.addCard(newcard);
+   }
+   
+   public void startGame() {
+	   hand = new Hand();
+	   dealStartingCard();
+   }
+   
+//   public void startRound() {
+//	   
+//   }
+   
+   public void dealStartingCard(){
+	   
+   }
 }
