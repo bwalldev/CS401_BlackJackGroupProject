913f34dc20a9676d38854724ccd349271b5d3f36
Pugnatorr <123401888+Pugnatorr@users.noreply.github.com>
2025-04-24 18:54:26 -0700

    Update Table.java
    Better start to table class

--- BlackjackGame/src/Table.java
+++ BlackjackGame/src/Table.java
@@ -1,11 +1,36 @@
 
 public class Table {
+  private ArrayList<Player> players;
   private Dealer dealer;
-  private Shoe shoe;
-  private int numPlayers; //number of players on the table
-  private List<Player> players;
-  private int tableID;
+  private int maxPlayers;
+  private int maxDealers;
+  private Game game;
 
-  public Table(int tableID) {
-    this.table
+  public Table(Dealer dealer) {
+    this.plaeyrs = new ArrayList<>();
+    this.dealer = dealer;
+    this.maxPlayers = 6; //6 players maximum per table
+    this.maxDealers = 2; //2 players maximum per table
+  }
+
+  public ArrayList<Player> getPlayers() {
+    return players;
+  }
+
+  public Dealer getDealer() {
+    return dealer;
+  }
+
+  public int getMaxPlayers() {
+    return maxPlayers;
+  }
+
+  public int getMaxDealers() {
+    return maxDealers;
+  }
+
+  public boolean isEmpty() {
+    return players.isEmpty();
+  }
+    
 }
