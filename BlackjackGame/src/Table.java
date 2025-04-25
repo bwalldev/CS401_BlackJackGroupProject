1280081885e521b784ec5a46ea5aca9e3084a0c8
Pugnatorr <123401888+Pugnatorr@users.noreply.github.com>
2025-04-24 18:42:25 -0700

    Update Table.java
    sample need to review UML class diagram again be right back

--- BlackjackGame/src/Table.java
+++ BlackjackGame/src/Table.java
@@ -1,4 +1,11 @@
 
 public class Table {
+  private Dealer dealer;
+  private Shoe shoe;
+  private int numPlayers; //number of players on the table
+  private List<Player> players;
+  private int tableID;
 
+  public Table(int tableID) {
+    this.table
 }
