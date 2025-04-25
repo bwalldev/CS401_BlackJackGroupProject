91569400fc66fbd25f9c1802a87e16d7377ef75a
Brandon Wall <brandon.wall.dev@gmail.com>
2025-04-24 19:26:23 -0700

    Created ClientHandler class inside the Server class

--- BlackjackGame/src/Server.java
+++ BlackjackGame/src/Server.java
@@ -1,4 +1,36 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
 public class Server {
-
+	public static void main(String[] args) {
+		
+	}
+	
+	// -------------- ClientHandler Class ----------------------------------------------------
+	
+	class Clienthandler implements Runnable {
+		private final Socket clientSocket;
+		
+		public Clienthandler(Socket socket) {
+			this.clientSocket = socket;
+		}
+		
+	
+		@Override
+		public void run() {
+			ObjectOutputStream outStream = null;
+			ObjectInputStream inStream = null;
+			
+			try {
+				outStream = new ObjectOutputStream(clientSocket.getOutputStream());
+				inStream = new ObjectInputStream(clientSocket.getInputStream());
+				
+			} catch (IOException except) {
+				except.printStackTrace();
+			}
+		}
+		
+	}
 }
