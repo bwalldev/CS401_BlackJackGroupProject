e523e7adf2b30a061d36661f28c237a35a471997
Brandon Wall <brandon.wall.dev@gmail.com>
2025-04-27 20:21:21 -0700

    Modified Message class

--- BlackjackGame/src/Message.java
+++ BlackjackGame/src/Message.java
@@ -2,24 +2,29 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class Message implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private static int idCount = 0;
 	private final int id;
-	private final String type;
+	private final MessageType type;
 	private final String text;
+	private final String from;
 	private final Date date;
+	private final Card card;
 	
-	public Message(String text, String type) {
-		this.id = ++this.idCount;
+	public Message(MessageType type, String username, String password, String text, String from, Card card) {
+		this.id = ++Message.idCount;
 		this.type = type;
 		this.text = text;
+		this.from = from;
 		this.date = new Date();
+		this.card = card;
 	}
 	
 	public int getID() {
 		return this.id;
 	}
 	
-	public String getType() {
+	public MessageType getType() {
 		return this.type;
 	}
 	
@@ -29,5 +34,13 @@ public class Message implements Serializable {
 	
 	public Date getDate() {
 		return this.date;
+	}
+	
+	public String getFrom() {
+		return this.from;
+	}
+	
+	public Card getCard() {
+		return this.card;
 	}
 }
