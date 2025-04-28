f8bce74c56fafd7478609acd960f3e7ed8cdf9d2
Brandon Wall <brandon.wall.dev@gmail.com>
2025-04-27 22:49:49 -0700

    Fixed spacing style

--- BlackjackGame/src/Game.java
+++ BlackjackGame/src/Game.java
@@ -2,39 +2,37 @@ import java.util.ArrayList;
 import java.util.Timer;
 
 public class Game {
-    private ArrayList<Player> players;
-    private Dealer dealer;
-    private Shoe shoe;
-    private int currentPlayer;
-    private Timer timer;
-    private int maxPlayers;
+	private ArrayList<Player> players;
+	private Dealer dealer;
+	private Shoe shoe;
+	private int currentPlayer;
+	private Timer timer;
+	private int maxPlayers;
 
-    public Game(ArrayList<Player> players, Dealer dealer, int maxPlayers){
-        this.players = players;
-        this.dealer = dealer;
-        this.maxPlayers = maxPlayers;
-        this.shoe = new Shoe(3); // 3 decks in the shoe
-        this.currentPlayer = 0;
-        this.timer = new Timer();
-    }
-  
-    public Shoe getShoe() {
-	    return this.shoe;
+	public Game(ArrayList<Player> players, Dealer dealer, int maxPlayers){
+		this.players = players;
+		this.dealer = dealer;
+		this.maxPlayers = maxPlayers;
+		this.shoe = new Shoe(3); // 3 decks in the shoe
+		this.currentPlayer = 0;
+		this.timer = new Timer();
 	}
-  
-    public void nextPlayerTurn() {
-  
-    }
-  
-    public void dealerTurn() {
-    
-    }
-    
-    public void playerTurn() {
-  
-    }
-    
-    public void roundEnd() {
-  
-    }   
+
+	public Shoe getShoe() {
+		return this.shoe;
+	}
+
+	public void nextPlayerTurn() {
+	}
+	
+	public void dealerTurn() {
+	}
+	
+	public void playerTurn() {
+		
+	}
+	
+	public void roundEnd() {
+		
+	}
 }
--- BlackjackGame/src/Table.java
+++ BlackjackGame/src/Table.java
@@ -1,17 +1,17 @@
 import java.util.ArrayList;
 
 public class Table {
-    private ArrayList<Player> players;
-    private Dealer dealer;
-    private int maxPlayers;
-    private int maxDealers;
-    private Game game;
+	private ArrayList<Player> players;
+	private Dealer dealer;
+	private int maxPlayers;
+	private int maxDealers;
+	private Game game;
 
-    public Table(Dealer dealer) {
-	    this.players = new ArrayList<>();
-	    this.dealer = dealer;
-	    this.maxPlayers = 6; //6 Players maximum per table
-	    this.maxDealers = 1; //1 Dealer maximum per table
+	public Table(Dealer dealer) {
+		this.players = new ArrayList<>();
+		this.dealer = dealer;
+		this.maxPlayers = 6; //6 Players maximum per table
+		this.maxDealers = 1; //1 Dealer maximum per table
     }
 
     public ArrayList<Player> getPlayers() {
@@ -29,12 +29,12 @@ public class Table {
     public int getMaxDealers() {
     	return maxDealers;
     }
-  
+    
     public Game getGame() {
-	    return this.game;
+    	return this.game;
     }
 
     public boolean isEmpty() {
-        return players.isEmpty();
-    }  
+    	return players.isEmpty();
+    }
 }
