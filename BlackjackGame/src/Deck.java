4bb28bedb9aa73be337b9c3a76c5ade2a2496da8
Brandon Wall <brandon.wall.dev@gmail.com>
2025-04-27 22:38:37 -0700

    Changed fillDeck() and fixed spacing style in Table file to 4 spaces for indentation

--- BlackjackGame/src/Deck.java
+++ BlackjackGame/src/Deck.java
@@ -6,6 +6,7 @@ public class Deck {
 	protected List<Card> cards;
 	protected boolean empty;
 	
+	// Constructor
 	public Deck() {
 		this.cards = new ArrayList<Card>();
 		
@@ -34,77 +35,10 @@ public class Deck {
 	}
 	
 	protected void fillDeck() {
-		Suit mySuit = null;
-		Symbol mySymbol = null;
-		
-		//loop for the number of different suites
-		for(int i = 0; i < Suit.values().length; i++) {
-			//set up our suit based on the loop counter
-			switch(i) {
-				case(0):
-					mySuit = Suit.SPADE;
-					break;
-				case(1):
-					mySuit = Suit.CLOVER;
-					break;
-				case(2):
-					mySuit = Suit.HEART;
-					break;
-				case(3):
-					mySuit = Suit.DIAMOND;
-					break;
-				default:
-					System.out.println("suit not properly determined in deck");
-			}
-			
-			//loop for the number of different symbols
-			for(int ii = 0; ii < Symbol.values().length; ii++) {
-				switch(ii) {
-					case(0):
-						mySymbol = Symbol.ACE;
-						break;
-					case(1):
-						mySymbol = Symbol.TWO;
-						break;
-					case(2):
-						mySymbol = Symbol.THREE;
-						break;
-					case(3):
-						mySymbol = Symbol.FOUR;
-						break;
-					case(4):
-						mySymbol = Symbol.FIVE;
-						break;
-					case(5):
-						mySymbol = Symbol.SIX;
-						break;
-					case(6):
-						mySymbol = Symbol.SEVEN;
-						break;
-					case(7):
-						mySymbol = Symbol.EIGHT;
-						break;
-					case(8):
-						mySymbol = Symbol.NINE;
-						break;
-					case(9):
-						mySymbol = Symbol.TEN;
-						break;
-					case(10):
-						mySymbol = Symbol.JACK;
-						break;
-					case(11):
-						mySymbol = Symbol.QUEEN;
-						break;
-					case(12):
-						mySymbol = Symbol.KING;
-						break;
-					default:
-						System.out.println("symbol not properly determined in deck");
-				}
-				
-				//add a new card to the list based on the suit, symbol pair
-				this.cards.add(new Card(mySuit, mySymbol));
+		// Creates every card with every symbol variant
+		for(Suit suit : Suit.values()) {
+			for(Symbol symbol : Symbol.values()) {
+				this.cards.add(new Card(suit, symbol));
 			}
 		}
 	}
--- BlackjackGame/src/Game.java
+++ BlackjackGame/src/Game.java
@@ -2,39 +2,39 @@ import java.util.ArrayList;
 import java.util.Timer;
 
 public class Game {
-  private ArrayList<Player> players;
-  private Dealer dealer;
-  private Shoe shoe;
-  private int currentPlayer;
-  private Timer timer;
-  private int maxPlayers;
+    private ArrayList<Player> players;
+    private Dealer dealer;
+    private Shoe shoe;
+    private int currentPlayer;
+    private Timer timer;
+    private int maxPlayers;
 
-  public Game(ArrayList<Player> players, Dealer dealer, int maxPlayers){
-    this.players = players;
-    this.dealer = dealer;
-    this.maxPlayers = maxPlayers;
-    this.shoe = new Shoe();
-    this.currentPlayer = 0;
-    this.timer = new Timer();
-  }
-
+    public Game(ArrayList<Player> players, Dealer dealer, int maxPlayers){
+        this.players = players;
+        this.dealer = dealer;
+        this.maxPlayers = maxPlayers;
+        this.shoe = new Shoe(3); // 3 decks in the shoe
+        this.currentPlayer = 0;
+        this.timer = new Timer();
+    }
   
-  public Shoe getShoe() {
+    public Shoe getShoe() {
 	    return this.shoe;
 	}
   
-  public void nextPlayerTurn() {
+    public void nextPlayerTurn() {
   
-  }
+    }
   
-  public void dealerTurn() {
+    public void dealerTurn() {
     
-  }
-  public void playerTurn() {
-  
-  }
-  public void roundEnd() {
-  
-  }  
+    }
     
+    public void playerTurn() {
+  
+    }
+    
+    public void roundEnd() {
+  
+    }   
 }
