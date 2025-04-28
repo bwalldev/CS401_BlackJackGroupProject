3d0d6690dfe285629a200446a825a0589d35e29d
Brandon Wall <brandon.wall.dev@gmail.com>
2025-04-27 21:59:24 -0700

    Modified Deck and Shoe

--- BlackjackGame/src/Shoe.java
+++ BlackjackGame/src/Shoe.java
@@ -1,15 +1,38 @@
+import java.util.ArrayList;
 import java.util.Collections;
 
 public class Shoe extends Deck {
+	private int numDecks;
+	
+	public Shoe(int numDecks) {
+		this.numDecks = numDecks;
+		this.cards = new ArrayList<Card>();
+		
+		for (int i = 0; i < this.numDecks; i++) {
+			fillDeck();
+		}
+		
+		Collections.shuffle(this.cards);
+		
+		this.empty = false;
+	}
+	
+	public int getNumDecks() {
+		return this.numDecks;
+	}
 
-	protected void resetDeck() {
+	protected void reset() {
 		//clear the deck
 		this.cards.clear();
+		
 		//call helper function to fill the deck full of cards three times
-		for(int i=0;i<3;i++) {
+		for(int i = 0; i < this.numDecks; i++) {
 			fillDeck();	
 		}
+		
 		//randomly shuffle the items in the deck
 		Collections.shuffle(this.cards);
+		
+		this.empty = false;
 	}
 }
