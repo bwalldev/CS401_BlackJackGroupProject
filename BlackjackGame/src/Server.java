348539f7394303655762128fce7dcf6c7db40a85
Armand Tirado <armandfunnyman@yahoo.com>
2025-04-28 11:06:39 -0700

    server updated
    server can now connect to clients. server socket should probably be changed.

--- BlackjackGame/src/Server.java
+++ BlackjackGame/src/Server.java
@@ -1,4 +1,87 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+	public static void main(String[] args) {
+		
+		ServerSocket server = null;
 
+		System.out.println("server running");
+
+		try {
+
+			// server is listening on port 1234
+			server = new ServerSocket(1234);
+			server.setReuseAddress(true);
+
+			// running infinite loop for getting
+			// client request
+			while (true) {
+
+				// socket object to receive incoming client
+				// requests
+				Socket client = server.accept();
+
+				// Displaying that new client is connected
+				// to server
+				System.out.println("New client connected");
+
+				// create a new thread object
+				ClientHandler clientSock
+					= new ClientHandler(client);
+
+				// This thread will handle the client
+				// separately
+				new Thread(clientSock).start();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (server != null) {
+				try {
+					server.close();
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	// -------------- ClientHandler Class ----------------------------------------------------
+
+	private static class ClientHandler implements Runnable {
+		private final Socket clientSocket;
+		
+		public ClientHandler(Socket socket) {
+			this.clientSocket = socket;
+		}
+		
+
+		@Override
+		public void run() {
+			ObjectOutputStream outStream = null;
+			ObjectInputStream inStream = null;
+			
+			try {
+				outStream = new ObjectOutputStream(clientSocket.getOutputStream());
+				inStream = new ObjectInputStream(clientSocket.getInputStream());
+				
+			} catch (IOException except) {
+				except.printStackTrace();
+			}
+		}
+		
+	}
+
+
+
+//	@Override
+//	public void run() {
+//	}
 }
