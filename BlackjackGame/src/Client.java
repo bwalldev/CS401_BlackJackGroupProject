1a41af0501ffdc2ddc9ee08a486f1833186b7421
Brandon Wall <brandon.wall.dev@gmail.com>
2025-04-29 21:44:50 -0700

    Copied Client from main branch

--- BlackjackGame/src/Client.java
+++ BlackjackGame/src/Client.java
@@ -1,34 +1,58 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) {
-    	ObjectInputStream inStream = null;
-    	ObjectOutputStream outStream = null;
-    	
-    	try {
-    		Socket socket = new Socket("localhost", 5555);
-    		
-    		inStream = new ObjectInputStream(socket.getInputStream());
-    		outStream = new ObjectOutputStream(socket.getOutputStream());
-    	} catch (IOException except) {
-    		except.printStackTrace();
-    	}
+    private Socket socket;                  
+    private BufferedReader input;          
+    private PrintWriter output;            
+                    
+
+    //connect the client to the server on localhost
+    public void connectToServer(int port) {
+       try {
+    	   socket= new Socket ("localhost", port);
+    	   input = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+    	   output = new PrintWriter(socket.getOutputStream(), true);
+       } catch (IOException e) {
+    	   System.out.println("connection faild! " +  e.getMessage());
+       }
     }
-  
+
+   // send message to server
     public void send(String message) {
+    	if (output!= null)
+    		output.println(message);  
     }
-
+    
+   // receives message from server
     public String receive(){
-		return null;
+    	try {
+    		if (input != null) {
+    			return input.readLine();
+    		}
+    		else {
+    			return null;
+    		}
+    	} catch (IOException e) {
+    		System.out.println("receiving faild! " + e.getMessage());
+    		return null;
+    		
+    	}  
     }
 
-    //sends players action ( hit, stand)
-    public void sendAction(String action) {
-    }
-
+    //disconnects from server
     public void disconnect() {
+    	try {
+    		 if (input != null) input.close();
+             if (output != null) output.close();
+             System.out.println("Disconnected! ");
+    	}
+    	catch(IOException e) {
+            System.out.println("Error Disconnecting! " + e.getMessage());
+    	}	
+       
     }
 }
