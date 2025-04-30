6ed973f592128d006d795ad7fba0633649ce3867
Mahlet2425 <Mahletsolomon138@gmail.com>
2025-04-29 19:30:39 -0700

    updated client

--- BlackjackGame/src/Client.java
+++ BlackjackGame/src/Client.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -6,31 +8,51 @@ public class Client {
     private Socket socket;                  
     private BufferedReader input;          
     private PrintWriter output;            
-    private Player player;                 
-    private Game game;                     
+                    
 
-    //connect the client to the server
-    public void connectToServer(String serverAddress, int port) {
-       
+    //connect the client to the server on localhost
+    public void connectToServer(int port) {
+       try {
+    	   socket= new Socket ("localhost", port);
+    	   input = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+    	   output = new PrintWriter(socket.getOutputStream(), true);
+       } catch (IOException e) {
+    	   System.out.println("connection faild! " +  e.getMessage());
+       }
     }
 
-  
+   // send message to server
     public void send(String message) {
-       
+    	if (output!= null)
+    		output.println(message);  
     }
-
+    
+   // receives message from server
     public String receive(){
-    	
-		return null;
-        
+    	try {
+    		if (input != null) {
+    			return input.readLine();
+    		}
+    		else {
+    			return null;
+    		}
+    	} catch (IOException e) {
+    		System.out.println("receiving faild! " + e.getMessage());
+    		return null;
+    		
+    	}  
     }
 
-    //sends players action ( hit, stand)
-    public void sendAction(String action) {
-       
-    }
-
+    //disconnects from server
     public void disconnect() {
+    	try {
+    		 if (input != null) input.close();
+             if (output != null) output.close();
+             System.out.println("Disconnected! ");
+    	}
+    	catch(IOException e) {
+            System.out.println("Error Disconnecting! " + e.getMessage());
+    	}	
        
     }
 }
