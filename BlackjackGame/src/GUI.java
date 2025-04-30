1d399cb740b282daa8d6b13895dd428a52d66ad2
Pugnatorr <123401888+Pugnatorr@users.noreply.github.com>
2025-04-30 16:25:27 -0700

    What I have so far on the GUI madethe window and started on the message panel

--- BlackjackGame/src/GUI.java
+++ BlackjackGame/src/GUI.java
@@ -1,15 +1,32 @@
 import java.awt.CardLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class GUI extends JFrame {
 	private CardLayout cardLayout;
 	private JPanel panel;
+	private Game game;
+	private JTextArea messageArea;
+	private JButton hitButton, stayButton, startButton;
 	
 	public GUI() {
-		this.setTitle("Welcome to G2 BlackJack");
-		this.setSize(800, 500);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//create the window 
+		this.setTitle("BlackJack Game"); //title
+		this.setSize(800, 500); //size of panel 
+		this.setLocationRelativeTo(null); //relative to change
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //close the window when you hit close
+		this.setLayout(new BorderLayout());
+		
+		//create message panel 
+		messageArea = new JTextArea();
+		messageArea.setEditable(false);
+		messageArea.setFont(new Font("Times New Roman", Font.PLAIN, 16)); //this is cool ngl
+		JScrollPane scrollPane = new JScrollPane(messageArea);
+		add(scrollPane, BorderLayout.CENTER);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		
 	}
 }
