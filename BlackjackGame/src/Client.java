4d09ed091edd1e44d2e8d9dccb07d902f50cd138
Brandon Wall <brandon.wall.dev@gmail.com>
2025-04-29 18:59:32 -0700

    Added GUI class and modified Client

--- BlackjackGame/src/Client.java
+++ BlackjackGame/src/Client.java
@@ -1,36 +1,34 @@
-import java.io.BufferedReader;
-import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class Client {
-    private Socket socket;                  
-    private BufferedReader input;          
-    private PrintWriter output;            
-    private Player player;                 
-    private Game game;                     
-
-    //connect the client to the server
-    public void connectToServer(String serverAddress, int port) {
-       
+    public static void main(String[] args) {
+    	ObjectInputStream inStream = null;
+    	ObjectOutputStream outStream = null;
+    	
+    	try {
+    		Socket socket = new Socket("localhost", 5555);
+    		
+    		inStream = new ObjectInputStream(socket.getInputStream());
+    		outStream = new ObjectOutputStream(socket.getOutputStream());
+    	} catch (IOException except) {
+    		except.printStackTrace();
+    	}
     }
-
   
     public void send(String message) {
-       
     }
 
     public String receive(){
-    	
 		return null;
-        
     }
 
     //sends players action ( hit, stand)
     public void sendAction(String action) {
-       
     }
 
     public void disconnect() {
-       
     }
 }
