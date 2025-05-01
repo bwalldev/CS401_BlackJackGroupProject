5ec376313ad544c4787e99b693ff060311890160
Pugnatorr <123401888+Pugnatorr@users.noreply.github.com>
2025-05-01 13:05:40 -0700

    found out you can create an enum instance wth 2 values for ace but determining which value to be acllled in the case of the game is where im currently stuck

--- BlackjackGame/src/Symbol.java
+++ BlackjackGame/src/Symbol.java
@@ -1,7 +1,7 @@
 // Enumerator used in the Card class
 
 public enum Symbol {
-	ACE(1), 
+	ACE(1, 10),  //Ace has value of either 1 or 10
 	TWO(2), 
 	THREE(3),
 	FOUR(4),
@@ -10,8 +10,19 @@ public enum Symbol {
 	SEVEN(7),
 	EIGHT(8),
 	NINE(9),
-	TEN(10),
+	TEN(10), //10 & all facecards have value of 10
 	JACK(10),
 	QUEEN(10),
 	KING(10);
+
+	//counter for value of ace in array 1 or 10
+	private final int[] aceValue;
+
+	//Constructor for the ace value in game
+	Symbol(int[] aceValue) {
+		this.aceValue = aceValue;
+	}
+
+	
+	
 }
