39b55aed166a0ee9466a9e0777765383f8b02cc3
Brandon Wall <brandon.wall.dev@gmail.com>
2025-05-06 17:12:07 -0700

    Modified Server notifications

--- BlackjackGame/src/Server.java
+++ BlackjackGame/src/Server.java
@@ -291,6 +291,8 @@ public class Server {
 							Player newPlayer = new Player(username, password, balance);
 							Server.loggedInPlayers.add(newPlayer);
 							
+							System.out.println("Player " + username + " has logged in.");
+							
 							Message success = new Message(MessageType.LOGIN, username, password, balance, "Login successful.", null, null, -1 );
 		                    this.outStream.writeObject(success);
 		                    
@@ -321,6 +323,8 @@ public class Server {
 							if (getLoggedInDealer(username) == null) {
 								Dealer newDealer = new Dealer(username, password);
 								Server.loggedInDealers.add(newDealer);
+								
+								System.out.println("Dealer " + username + " has logged in");
 								
 								Message success = new Message(MessageType.LOGIN, username, password, 0, "Dealer login successful.", null, null, -1);
 								this.outStream.writeObject(success);
